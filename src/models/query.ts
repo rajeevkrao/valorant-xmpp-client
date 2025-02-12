a8a36ea5d1ffe1f860172a780c581f83d239cc0c
Rajeev K Rao <rajeevraok@gmail.com>
2025-02-12 16:00:45 +0530

    Models Update

---
+++ src/models/query.ts
@@ -0,0 +1,8 @@
+import { ItemObject } from "./item"
+
+export interface QueryObject {
+	item?: ItemObject | ItemObject[]
+	xmlns: QueryTypes
+}
+
+export type QueryTypes = "jabber:iq:riotgames:roster"
