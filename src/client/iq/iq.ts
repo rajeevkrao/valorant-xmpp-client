573361f2924f8e093806073b35391317f294eedf
Rajeev K Rao <rajeevraok@gmail.com>
2025-02-12 16:02:04 +0530

    Iq Functions

--- src/client/iq/iq.ts
+++ src/client/iq/iq.ts
@@ -1,25 +1,22 @@
+import type { IqObject } from "../../models/iq";
+import type { QueryObject, QueryTypes } from "../../models/query";
 import { JidObject } from "../valorant-xmpp";
 
-export const formatIq = (presence: IqInput): IqOutput => {
-  const { from, to, type, id, query, ...other } = presence;
+export const formatIq = (iqData: IqObject): IqOutput => {
+  const { from, to, type, id, query, ...other } = iqData;
   return {
     type,
+    queryType: query?.xmlns,
+    query,
   };
 };
-
-export interface IqInput {
-  from?: string;
-  to?: string;
-  type?: string;
-  query?: any;
-  id?: string;
-  [propName: string]: any;
-}
 
 export interface IqOutput {
   /* sender: JidObject;
   recipient: JidObject; */
+  queryType: QueryTypes;
   type: string;
+  query?: QueryObject;
   /* id: string | null;
   [propName: string]: any; */
 }
